6e765cb16c0bcd7b6b5284e91781c5a1aa79c2a7
HARI NARAYAN SHUKLA <hari7706shukla@gmail.com>
2023-07-27 15:13:17 +0530

    first commit

---
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,49 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace CodePulse.API.Controllers
+{
+    //https://localhost:xxxx/api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            //Map DtO to domain model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle,
+
+            };
+            
+            await categoryRepository.CreateAsync(category);
+
+            //domain model to dto not exposed directly
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+
+            };
+            return Ok(response);
+        }
+    }
+}
